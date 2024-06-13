2891043e58d8147565bd91a29225711328f7b179
systemheng <thatstrending0@gmail.com>
2024-06-13 18:00:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let transis = document.getElementsByClassName("heading")[0];
+    transis.style.left = "10%";
+}
+)
+document.addEventListener("DOMContentLoaded", () => {
+    let subheadtransis = document.getElementsByClassName("subheading")[0];
+    subheadtransis.style.left = "10%"
+})
